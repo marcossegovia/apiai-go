fc070ce31b7f54fe30f41d4a97bb6da5fd41f600
Wyatt Arent <hello@wyatt.engineer>
2018-02-24 22:42:24 -0800

    Improve error message

--- query.go
+++ query.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"net/url"
 	"time"
@@ -141,6 +142,7 @@ func (c *ApiClient) Query(q Query) (*QueryResponse, error) {
 		}
 		return response, nil
 	default:
-		return nil, fmt.Errorf("apiai: wops something happens because status code is %v", resp.StatusCode)
+		body, _ := ioutil.ReadAll(resp.Body)
+		return nil, fmt.Errorf("ApiAi Error: %v: %v", resp.StatusCode, string(body))
 	}
 }
